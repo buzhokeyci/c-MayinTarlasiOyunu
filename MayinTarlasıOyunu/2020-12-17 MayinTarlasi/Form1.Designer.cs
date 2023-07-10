528fb8b05abb80f0ad23bdcef889e03a6af96909
Enes Mert Kaya <127692367+buzhokeyci@users.noreply.github.com>
2023-07-10 10:36:30 +0300

    mayın tarlası oyunu
    C# ile 3 zorlukta mayın tarlası oyunu

---
+++ MayinTarlasıOyunu/2020-12-17 MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace _2020_12_17_MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kolayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ortaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpTarla = new System.Windows.Forms.GroupBox();
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(812, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(64, 26);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // yeniToolStripMenuItem
+            // 
+            this.yeniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kolayToolStripMenuItem,
+            this.ortaToolStripMenuItem,
+            this.zorToolStripMenuItem});
+            this.yeniToolStripMenuItem.Name = "yeniToolStripMenuItem";
+            this.yeniToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.yeniToolStripMenuItem.Text = "Yeni";
+            // 
+            // kolayToolStripMenuItem
+            // 
+            this.kolayToolStripMenuItem.Name = "kolayToolStripMenuItem";
+            this.kolayToolStripMenuItem.Size = new System.Drawing.Size(129, 26);
+            this.kolayToolStripMenuItem.Text = "Kolay";
+            this.kolayToolStripMenuItem.Click += new System.EventHandler(this.Seviye_Click);
+            // 
+            // ortaToolStripMenuItem
+            // 
+            this.ortaToolStripMenuItem.Name = "ortaToolStripMenuItem";
+            this.ortaToolStripMenuItem.Size = new System.Drawing.Size(129, 26);
+            this.ortaToolStripMenuItem.Text = "Orta";
+            this.ortaToolStripMenuItem.Click += new System.EventHandler(this.Seviye_Click);
+            // 
+            // zorToolStripMenuItem
+            // 
+            this.zorToolStripMenuItem.Name = "zorToolStripMenuItem";
+            this.zorToolStripMenuItem.Size = new System.Drawing.Size(129, 26);
+            this.zorToolStripMenuItem.Text = "Zor";
+            this.zorToolStripMenuItem.Click += new System.EventHandler(this.Seviye_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.Cikis_Click);
+            // 
+            // grpTarla
+            // 
+            this.grpTarla.Location = new System.Drawing.Point(12, 78);
+            this.grpTarla.Name = "grpTarla";
+            this.grpTarla.Size = new System.Drawing.Size(776, 334);
+            this.grpTarla.TabIndex = 1;
+            this.grpTarla.TabStop = false;
+            this.grpTarla.Text = "Tarla";
+            this.grpTarla.Enter += new System.EventHandler(this.grpTarla_Enter);
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.AutoSize = true;
+            this.lblPuan.Location = new System.Drawing.Point(39, 45);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(0, 17);
+            this.lblPuan.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 444);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.grpTarla);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kolayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ortaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zorToolStripMenuItem;
+        private System.Windows.Forms.GroupBox grpTarla;
+        private System.Windows.Forms.Label lblPuan;
+    }
+}
+
---
+++ MayinTarlasıOyunu/2020-12-17 MayinTarlasi/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020_12_17_MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int seviye;
+        private void Seviye_Click(object sender, EventArgs e)
+        {
+            //önceki kontrolleri temizle
+            grpTarla.Controls.Clear();
+            //foreach (Control item in grpTarla.Controls)
+            //{
+            //    grpTarla.Controls.Remove(item);
+            //}
+
+            //var asdf = sender.GetType().ToString();
+            //bir nesnenin çalışma anında tipini öğrenmek için kullanılır.
+
+            ToolStripMenuItem tsmi = sender as ToolStripMenuItem;
+
+            switch (tsmi.Text)
+            {
+                case "Kolay":
+                    seviye = 1;
+                    break;
+                case "Orta":
+                    seviye = 2;
+                    break;
+                case "Zor":
+                    seviye = 3;
+                    break;
+            }
+
+            Random rnd = new Random();
+            int[,] mayinKoordinatlari = new int[10 * seviye, 2];
+            //çok boyutlu diziler
+            //aşağıdaki örnekte gördüğünüz int[3,2] için 3 toplam küme sayısı, 2 ise kümelerin içinde barındıracağı eleman sayısı
+            //int[,] asdf2 = new int[3, 2]
+            //int[,] x = new int[10, 2]
+            //int[,] b = { { 3, 5 }, { 1, 1 }, { 3, 4 } };
+            //b[0, 0] = 3
+            //b[0, 1] = 5
+            //b[1, 0] = 1
+            //b[1, 1] = 1
+            //b[2, 0] = 3
+            //b[2, 1] = 4
+
+            for (int i = 0; i < (10 * seviye); i++)
+            {
+                mayinKoordinatlari[i, 0] = rnd.Next(0, seviye * 10);
+                mayinKoordinatlari[i, 1] = rnd.Next(0, seviye * 10);
+            }
+
+            int olcu = 20;
+            for (int y = 0; y < seviye * 10; y++)
+            {
+                for (int x = 0; x < seviye * 10; x++)
+                {
+                    Button kutu = new Button();
+                    kutu.Height = olcu;
+                    kutu.Width = olcu;
+                    kutu.Top = y * olcu + 20;
+                    kutu.Left = x * olcu;
+                    kutu.BackColor = Color.White;
+                    kutu.Click += Kutu_Click;
+
+                    kutu.Tag = false;
+                    for (int i = 0; i < 10 * seviye; i++)
+                    {
+                        if (mayinKoordinatlari[i, 0] == x && mayinKoordinatlari[i, 1] == y)
+                        {
+                            kutu.Tag = true;
+                            break;
+                        }
+                    }
+
+                    grpTarla.Controls.Add(kutu);
+                }
+            }
+
+            //groupbox'ın boyutlarını içindeki butonlara göre ayarlayalım
+            grpTarla.Width = seviye * 10 * olcu;
+            grpTarla.Height = seviye * 10 * olcu + 20;
+
+            //formun boyutlarını içindeki groupboxın konumuna göre ayarlayalım
+            this.Width = grpTarla.Width;
+            this.Height = grpTarla.Height + grpTarla.Top;
+
+            grpTarla.Visible = true;
+
+            lblPuan.Text = "0";
+        }
+
+        private void Kutu_Click(object sender, EventArgs e)
+        {
+            Button tiklananKutu = sender as Button;
+            if (Convert.ToBoolean(tiklananKutu.Tag) == true) //mayın var
+            {
+                foreach (Control item in grpTarla.Controls) //diğer mayınları da ortaya çıkaralım
+                {
+                    if (Convert.ToBoolean(item.Tag) == true)
+                    {
+                        item.BackColor = Color.Red;
+                    }
+                }
+
+                MessageBox.Show("Yandınız! Tekrar dene....");
+
+                grpTarla.Visible = false;
+                lblPuan.Text = "0";
+            }
+            else // mayın yok
+            {
+                tiklananKutu.Enabled = false;
+                tiklananKutu.BackColor = Color.Blue;
+                lblPuan.Text = (Convert.ToInt32(lblPuan.Text) + 1).ToString();
+            }
+        }
+
+        private void Cikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AutoSize = true;
+            grpTarla.Visible = false;
+        }
+
+        private void grpTarla_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
